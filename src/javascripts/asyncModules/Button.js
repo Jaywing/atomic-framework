183d501e75c3c49bfdbceb305d367c6dc3483de3
jtwd <james@jtwd.co.uk>
2016-09-28 10:54:05 +0100

    Changes to the Button module (use a data attr instead of classname for config)

--- src/javascripts/asyncModules/Button.js
+++ src/javascripts/asyncModules/Button.js
@@ -4,26 +4,32 @@ export default class Button {
 
   constructor(el) {
     this.el = el
+    this.activeClass = 'is-active'
+    this.runOnce = el.hasAttribute('data-button-once')
     this.init()
-    console.log('Button has been initialised')
   }
 
   init() {
 
-    let toggleActive = function(e) {
+    let Btn = this // 'this' reference
 
-      e.preventDefault()
-      this.classList.toggle('is-active')
+    let toggleActive = function() {
 
-      // Loading buttons: remove click event so it can't be toggled!
-      if (this.classList.contains('Button--loading')) {
+      this.classList.toggle(Btn.activeClass)
+      this.active = this.classList.contains(Btn.activeClass) // is it active?
+      this.setAttribute('aria-pressed', this.active) // set 'aria' attr
+
+      // Run once: remove click event so it can't be toggled!
+      if (Btn.runOnce) {
         this.removeEventListener('click', toggleActive)
       }
 
     }
 
-    this.el.addEventListener('click', toggleActive)
-  }
+    this.el.setAttribute('aria-pressed', this.el.classList.contains(this.activeClass)) // init aria
+    this.el.addEventListener('click', toggleActive) // click event
 
+    console.log('Button has been initialised')
+  }
 
 }
