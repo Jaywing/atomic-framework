5b1a626894141f4f46d1b91dfd0f5fb421d84c59
jtwd <james@jtwd.co.uk>
2016-09-29 13:17:52 +0100

    Some doc styling enhancements

--- src/javascripts/asyncModules/Sticky.js
+++ src/javascripts/asyncModules/Sticky.js
@@ -5,11 +5,12 @@ export default class Sticky {
   constructor(el) {
     this.el = el
     this.init()
-    console.log('Sticky has been initialised')
   }
 
   init() {
     let sticky = new StickyJs('[data-module="Sticky"]')
+
+    console.log('Sticky has been initialised')
   }
 
 }
