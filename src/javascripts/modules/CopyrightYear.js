75252014821544278dd3b35823fa69925ac1a973
jtwd <james@jtwd.co.uk>
2017-04-06 10:19:05 +0100

    Added script to update copyright year automatically

---
+++ src/javascripts/modules/CopyrightYear.js
@@ -0,0 +1,10 @@
+export default function updateCopyrightYear(id) {
+  const dt = new Date
+  const curYear = dt.getYear() + 1900
+  const $cpYear = document.getElementById(id)
+  let cpYear
+  if ($cpYear) {
+    cpYear = parseInt($cpYear.innerText)
+    if (cpYear !== curYear) $cpYear.innerText = curYear
+  }
+}
