befe688973c454ac364e455d3ab1b71bf2e60abf
Daniel Peters <dapen_@outlook.com>
2017-11-28 15:38:29 +0000

    strip gulp

--- gulpfile.js/lib/getEnabledTasks.js
+++ gulpfile.js/lib/getEnabledTasks.js
@@ -1,33 +1,32 @@
-var compact = require('lodash/compact')
-var isEmpty = require('lodash/isEmpty')
+var compact = require('lodash/compact');
+var isEmpty = require('lodash/isEmpty');
 
 // Grouped by what can run in parallel
-var assetTasks = ['fonts', 'iconFont', 'images', 'svgSprite']
-var codeTasks = ['html', 'stylesheets', 'javascripts']
+var assetTasks = ['fonts', 'iconFont', 'images', 'svgSprite'];
+var codeTasks = ['html', 'stylesheets', 'javascripts'];
 
 module.exports = function(env) {
-
   function matchFilter(task) {
-    if(TASK_CONFIG[task]) {
-      if(task === 'javascripts') {
-        task = env === 'production' ? 'webpack:production' : false
+    if (TASK_CONFIG[task]) {
+      if (task === 'javascripts') {
+        task = env === 'production' ? 'webpack:production' : false;
       }
-      return task
+      return task;
     }
   }
 
   function exists(value) {
-    return !!value
+    return !!value;
   }
 
   function findExistingTasks(candidates) {
-    var tasks = compact(candidates.map(matchFilter).filter(exists))
+    var tasks = compact(candidates.map(matchFilter).filter(exists));
 
-    return isEmpty(tasks) ? false : tasks
+    return isEmpty(tasks) ? false : tasks;
   }
 
   return {
     assetTasks: findExistingTasks(assetTasks),
     codeTasks: findExistingTasks(codeTasks)
-  }
-}
+  };
+};
--- gulpfile.js/tasks/rev/update-html.js
+++ gulpfile.js/tasks/rev/update-html.js
@@ -1,13 +1,14 @@
-if(!TASK_CONFIG.html) return false
+if (!TASK_CONFIG.html) return false;
 
-var gulp       = require('gulp')
-var revReplace = require('gulp-rev-replace')
-var path       = require('path')
+var gulp = require('gulp');
+var revReplace = require('gulp-rev-replace');
+var path = require('path');
 
 // 5) Update asset references in HTML
-gulp.task('update-html', function(){
-  var manifest = gulp.src(path.resolve(process.env.PWD, PATH_CONFIG.dest, "rev-manifest.json"))
-  return gulp.src(path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.html.dest, '**/*.html'))
+gulp.task('update-html', function() {
+  var manifest = gulp.src(path.resolve(process.env.PWD, PATH_CONFIG.dest, 'rev-manifest.json'));
+  return gulp
+    .src(path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.html.dest, '**/*.html'))
     .pipe(revReplace({ manifest: manifest }))
-    .pipe(gulp.dest(path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.html.dest)))
-})
+    .pipe(gulp.dest(path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.html.dest)));
+});
