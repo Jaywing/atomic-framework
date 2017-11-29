0fe7add68521b2d84dac1e74f5baec3f4d9c1d80
Daniel Peters <dapen_@outlook.com>
2017-11-29 13:24:29 +0000

    Adding missing dependencies

---
+++ config/task-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  html: {
+    excludeFolders: ['components', 'layouts', 'shared', 'macros', 'data']
+  },
+  images: true,
+  fonts: true,
+  static: true,
+  svgSprite: true,
+  ghPages: true,
+  stylesheets: true,
+
+  javascripts: {
+    entry: {
+      // files paths are relative to
+      // javascripts.dest in path-config.json
+      app: ['babel-polyfill', './app.js']
+    },
+    babel: {
+      presets: ['es2015', 'stage-1'],
+      plugins: ['transform-object-assign']
+    },
+    extractSharedJs: false
+  },
+
+  browserSync: {
+    server: {
+      // should match `dest` in
+      // path-config.json
+      baseDir: 'public'
+    }
+  },
+
+  production: {
+    rev: true
+  }
+};
