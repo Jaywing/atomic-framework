a165bec4212158adaa0995aef166d2ea10e3bcfb
Dapen <spartdan@live.co.uk>
2018-03-01 20:04:30 +0000

    webpack task done!

--- gulp/tasks/icons.js
+++ gulp/tasks/icons.js
@@ -1,4 +1,4 @@
-var gulp        = require('gulp');
+var gulp      = require('gulp');
 var svgstore  = require('gulp-svgstore')
 
 var CONFIG = require('../config.js');
--- gulp/tasks/webpack.js
+++ gulp/tasks/webpack.js
@@ -1,8 +1,36 @@
-var gulp = require('gulp');
-var webpack = require('gulp-webpack');
+var gulp      = require('gulp');
+var webpack   = require('webpack-stream');
+var webpack2  = require('webpack');
+var path      = require('path');
+
+var webpackConfig = {
+  context: path.resolve('js/'),
+  entry: {
+    app: ['babel-polyfill', 'app.js']
+  },
+  output: {
+    path: path.resolve('_build/js/'),
+    filename: 'app.js',
+    publicPath: '/js/'
+  },
+  plugins: [],
+  resolve: {
+    // extensions: extensions,
+    // alias: TASK_CONFIG.javascripts.alias,
+    modules: [path.resolve('js/'), path.resolve('node_modules')]
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        exclude: path.resolve('node_modules')
+      }
+    ]
+  }
+}
 
 gulp.task('webpack', function() {
   return gulp.src('./js/app.js')
-    .pipe(webpack())
-    .pipe(gulp.dest('./_build/'));
+    .pipe(webpack(webpackConfig, webpack2))
+    .pipe(gulp.dest('./_build/js/'));
 });
