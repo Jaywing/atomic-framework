fcbbab7878c2ee2248a157d3d582399013164134
Dapen <spartdan@live.co.uk>
2018-03-01 17:10:59 +0000

    clean and sass task

---
+++ gulp/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+
+    JS_FILES: [
+    'js/*.js'
+    ],
+  
+    JS_DEPS: [],
+  
+    JS_DOCS: [],
+  
+    SASS_DEPS_FILES: [
+      'node_modules/@(normalize-scss)/sass/**/*.scss'
+    ],
+
+    SASS_PATHS: [
+        'node_modules',
+        'scss'
+      ],
+  
+    SASS_DOC_PATHS: [
+      'node_modules',
+      'scss',
+      'docs/scss'
+    ],
+  
+    SASS_LINT_FILES: [
+      'scss/**/*.scss',
+    ],
+  
+    CSS_COMPATIBILITY: [
+      'last 2 versions',
+      'ie >= 9',
+      'Android >= 2.3',
+      'ios >= 7'
+    ],
+  
+    ASSETS_FILES: [
+      'docs/assets/**/*',
+      '!docs/assets/{js,scss}',
+      '!docs/assets/{js,scss}/**/*'
+    ],
+  
+    DIST_FILES: [],
+  };
---
+++ gulp/tasks/sass.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var fs = require('fs');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+var sassLint = require('gulp-sass-lint');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+var CONFIG = require('../config.js');
+
+// Compiles Sass files into CSS
+gulp.task('sass', ['sass:atomic', 'sass:docs']);
+
+// Prepare dependencies
+// gulp.task('sass:deps', function() {
+//   return gulp.src(CONFIG.SASS_DEPS_FILES)
+//     .pipe(gulp.dest('_vendor'));
+// });
+
+// Compiles Atomic Sass
+// gulp.task('sass:atomic', ['sass:deps'], function() {
+    gulp.task('sass:atomic', function() {
+  return gulp.src(['scss/atomic.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass({
+        includePaths: CONFIG.SASS_DOC_PATHS
+      }).on('error', sass.logError))
+    .pipe(postcss([autoprefixer({
+      browsers: CONFIG.CSS_COMPATIBILITY
+    })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('_build/assets/css'))
+    // .on('finish', function() {
+    //   gulp.src(CONFIG.SASS_LINT_FILES)
+    //     .pipe(sassLint({
+    //         config: './.sass-lint.yml'
+    //       }))
+    //     .pipe(sassLint.format());
+    // });
+});
+
+// Compiles docs Sass (includes Atomic code also)
+// gulp.task('sass:docs', ['sass:deps'], function() {
+    gulp.task('sass:docs', function() {
+  return gulp.src('docs/scss/docs.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: CONFIG.SASS_DOC_PATHS
+    }).on('error', sass.logError))
+    .pipe(postcss([autoprefixer({
+      browsers: CONFIG.CSS_COMPATIBILITY
+    })]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('_build/assets/css'));
+});
