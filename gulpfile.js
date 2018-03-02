05e386ddb16600da91e4cdce995fe94d733aadc7
Dapen <spartdan@live.co.uk>
2018-03-02 09:40:29 +0000

    add watch task from template to intergrate into framework tasks

--- gulpfile.js
+++ gulpfile.js
@@ -11,4 +11,12 @@ gulp.task('serve', ['build'], function(){
   browser.init({server: './_build', port: port, open: false});
 });
 
+gulp.task('watch', function() {
+  gulp.watch('./html/**/*', ['html:atomic', browser.reload]);
+  gulp.watch('./scss/**/*', ['sass:atomic', browser.reload]);
+  gulp.watch('./js/**/*', ['webpack', browser.reload]);
+  gulp.watch('./images/**/*', ['images:atomic', browser.reload]);
+  gulp.watch('./icons/*', ['icons:atomic', browser.reload]);
+});
+
 gulp.task('default', ['serve']);
